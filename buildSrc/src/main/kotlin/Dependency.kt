e25388648a0bf3257a97aa2c2143900bc6ebe899
Eric Roberto <58601124+ericDevRoberto@users.noreply.github.com>
2024-03-21 23:47:26 -0300

    libs organization

---
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -0,0 +1,17 @@
+object Dependency {
+
+    //Libs
+    const val core = "androidx.core:core-ktx:${Version.core}"
+    const val compat = "androidx.appcompat:appcompat:${Version.compat}"
+    const val googleMaterial = "com.google.android.material:${Version.googleMaterial}"
+    const val constraint = "androidx.constraintlayout:constraintlayout:${Version.constraint}"
+    const val lifeCycleLiveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Version.lifeCycle}"
+    const val lifeCycleViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Version.lifeCycle}"
+    const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${Version.navigation}"
+    const val navigationUi = "androidx.navigation:navigation-ui-ktx:${Version.navigation}"
+
+    //Test Libs
+    const val junit = "junit:junit:${Version.junit}"
+    const val junitTestExt = "androidx.test.ext:junit:${Version.junitTestExt}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Version.espresso}"
+}
---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,13 @@
+object Version {
+    const val core = "1.12.0"
+    const val compat = "1.6.1"
+    const val googleMaterial = "1.11.0"
+    const val constraint = "2.1.4"
+    const val lifeCycle = "2.7.0"
+    const val navigation = "2.7.7"
+
+
+    const val junit = "4.13.2"
+    const val junitTestExt = "1.1.5"
+    const val espresso = "3.5.1"
+}
